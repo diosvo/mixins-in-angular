1f05e65e451ff03cc8ec28d934591d7bd1dbb443
Dios Vo <vtmn1212@gmail.com>
2021-08-24 10:48:43 +0700

    ConfirmDialogComponent: -add Close button -fixed style -clean code

--- projects/mia-lib/src/lib/components/confirm-dialog/confirm-dialog.component.ts
+++ projects/mia-lib/src/lib/components/confirm-dialog/confirm-dialog.component.ts
@@ -4,10 +4,24 @@ import { IDialogPart } from './confirm-dialog.model';
 
 @Component({
   selector: 'confirm-dialog',
-  templateUrl: './confirm-dialog.component.html'
+  templateUrl: './confirm-dialog.component.html',
+  styles: [`
+    .dialog-title {
+      margin-bottom: 0.5rem;
+    }
+
+    .dialog-actions {
+      padding: 0.5rem 0 1rem;
+    }
+
+    .close-button {
+      margin: -1rem -1rem 0 0;
+    }
+  `]
 })
 export class ConfirmDialogComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) readonly data: { dialogPart: IDialogPart }) { }
-
+  constructor(
+    @Inject(MAT_DIALOG_DATA) readonly data: { dialogPart: IDialogPart }
+  ) { }
 }

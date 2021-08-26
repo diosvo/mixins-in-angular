b959bbeb6f70b9017cd39e4eb948e40faebd569a
Dios Vo <vtmn1212@gmail.com>
2021-08-26 21:22:19 +0700

    Temp UT for ToolbarComponent

--- src/app/home/components/toolbar/toolbar.component.spec.ts
+++ src/app/home/components/toolbar/toolbar.component.spec.ts
@@ -1,5 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
@@ -8,9 +13,17 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ToolbarComponent],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatDialogModule
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {

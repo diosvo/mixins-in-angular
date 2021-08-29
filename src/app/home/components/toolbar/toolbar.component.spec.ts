02e66eb070aeecc75542f60aacef0562bcb0849f
Dios Vo <vtmn1212@gmail.com>
2021-08-29 15:27:45 +0700

    ToolbarComponent: make UT 100%

--- src/app/home/components/toolbar/toolbar.component.spec.ts
+++ src/app/home/components/toolbar/toolbar.component.spec.ts
@@ -1,15 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  let dialog: MatDialog;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +24,12 @@ describe('ToolbarComponent', () => {
         MatToolbarModule,
         MatDialogModule
       ],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: { open: () => of() }
+        }
+      ]
     })
       .compileComponents();
   });
@@ -29,10 +37,33 @@ describe('ToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
+    dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('should call', () => {
+    test('openLoginDialog()', () => {
+      jest.spyOn(dialog, 'open');
+      component.openLoginDialog();
+      expect(dialog.open).toHaveBeenCalled();
+    });
+
+    describe('openLogoutDialog() when result returns', () => {
+      test('true', () => {
+        jest.spyOn(dialog, 'open').mockReturnValue({ afterClosed: () => of(true) } as MatDialogRef<typeof ToolbarComponent>);;
+        component.openLogoutDialog();
+        expect(dialog.open).toHaveBeenCalled();
+      });
+
+      test('false', () => {
+        jest.spyOn(dialog, 'open').mockReturnValue({ afterClosed: () => of(false) } as MatDialogRef<typeof ToolbarComponent>);;
+        component.openLogoutDialog();
+        expect(dialog.open).toHaveBeenCalled();
+      });
+    });
   });
 });

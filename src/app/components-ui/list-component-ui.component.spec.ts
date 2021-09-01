23dbd964da3a01add1c1cd06a7b7707e33172776
Dios Vo <vtmn1212@gmail.com>
2021-09-01 16:35:36 +0700

    ListComponentUiComponent: update UT

--- src/app/components-ui/list-component-ui.component.spec.ts
+++ src/app/components-ui/list-component-ui.component.spec.ts
@@ -1,4 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthModule } from '@auth/auth.module';
+import { MenuItemModule } from '@home/components/menu-item/menu-item.module';
+import { AlertModule } from '@lib/components/alert/alert.module';
 import { ListComponentUiComponent } from './list-component-ui.component';
 
 describe('ListComponentUiComponent', () => {
@@ -7,7 +22,29 @@ describe('ListComponentUiComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ListComponentUiComponent]
+      declarations: [ListComponentUiComponent],
+      imports: [
+        AuthModule,
+        AlertModule,
+        MenuItemModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatTooltipModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatProgressBarModule
+      ],
+      providers: [
+        {
+          provide: MatSnackBar,
+          useValue: {}
+        }
+      ]
     })
       .compileComponents();
   });
@@ -15,14 +52,28 @@ describe('ListComponentUiComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListComponentUiComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  describe('should create', () => {
-    try {
-      test ('hello', () => {
-        expect(component).toBeTruthy();
+  test('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('should call', () => {
+    test('onFormChanges()', async () => {
+      jest.spyOn(component, 'onFilters');
+      jest.spyOn(component, 'updateParams');
+
+      await component.onFormChanges();
+      component.componentsForm.setValue({
+        query: 'button',
+        group: 'button'
       });
-    } catch (e) { }
+
+      component.componentsForm.valueChanges.subscribe(_ => {
+        fixture.detectChanges();
+        expect(component.onFilters).toBeCalled();
+        expect(component.updateParams).toBeCalled();
+      });
+    });
   });
 });

3bb9334fb22e1a3ba7b213c6fe52dd3d36033aef
Dios Vo <vtmn1212@gmail.com>
2021-09-06 20:35:45 +0700

    Dialog style: config in global

--- projects/mia-lib/src/lib/components/confirm-dialog/confirm-dialog.component.ts
+++ projects/mia-lib/src/lib/components/confirm-dialog/confirm-dialog.component.ts
@@ -4,20 +4,7 @@ import { IDialogPart } from './confirm-dialog.model';
 
 @Component({
   selector: 'confirm-dialog',
-  templateUrl: './confirm-dialog.component.html',
-  styles: [`
-    .dialog-title {
-      margin-bottom: 0.5rem;
-    }
-
-    .dialog-actions {
-      padding: 0.5rem 0 1rem;
-    }
-
-    .close-button {
-      margin: -1rem -1rem 0 0;
-    }
-  `]
+  templateUrl: './confirm-dialog.component.html'
 })
 export class ConfirmDialogComponent {
 

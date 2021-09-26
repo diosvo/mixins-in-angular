c0cdf8d41ce1502d86bd70345cd517658905075b
Dios Vo <vtmn1212@gmail.com>
2021-09-26 21:34:58 +0700

    Refactor Unsaved Changes Guard

--- projects/mia-lib/src/lib/components/unsaved-changes-dialog/unsaved-changes-dialog.component.ts
+++ projects/mia-lib/src/lib/components/unsaved-changes-dialog/unsaved-changes-dialog.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-unsaved-changes-dialog',
@@ -19,26 +18,6 @@ import { Subject } from 'rxjs';
   `]
 })
 
-export class UnsavedChangesDialogComponent implements OnDestroy {
-  subject = new Subject<boolean>();
-
-  /**
-  * @description
-  * true => onDiscard() => go somewhere on the web
-  * false => onSave() => implements onSave() method
-  */
-
-  onSave(): void {
-    this.subject.next(true);
-    this.subject.complete();
-  }
-
-  onDiscard(): void {
-    this.subject.next(false);
-    this.subject.complete();
-  }
-
-  ngOnDestroy(): void {
-    this.subject.unsubscribe();
-  }
+export class UnsavedChangesDialogComponent {
+  constructor() { }
 }
--- projects/mia-lib/src/lib/guards/unsaved-changes.guard.ts
+++ projects/mia-lib/src/lib/guards/unsaved-changes.guard.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CanDeactivate, GuardsCheckEnd, Router } from '@angular/router';
 import { UnsavedChangesDialogComponent } from '@lib/components/unsaved-changes-dialog/unsaved-changes-dialog.component';
-import { Observable, of, Subject } from 'rxjs';
-import { filter, take } from 'rxjs/operators';
+import { SnackbarService } from '@lib/services/snackbar/snackbar.service';
+import { Observable, of } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
 import { DeactivateComponent } from '../models/base-form-component';
 
 @Injectable({
@@ -12,37 +13,38 @@ import { DeactivateComponent } from '../models/base-form-component';
 export class UnsavedChangesGuard implements CanDeactivate<DeactivateComponent> {
 
   constructor(
+    private router: Router,
     private dialog: MatDialog,
-    private router: Router
+    private snackbar: SnackbarService
   ) { }
 
-  canDeactivate(
-    component: DeactivateComponent
-  ): Observable<boolean> {
+  canDeactivate(component: DeactivateComponent): Observable<boolean> {
     if (!component.canDeactivate()) {
       const dialogRef = this.dialog.open(UnsavedChangesDialogComponent, {
         width: '500px',
         disableClose: true
       });
 
-      const subject = new Subject<boolean>();
-      dialogRef.componentInstance.subject = subject;
-
-      subject.subscribe((response) => {
-        if (response) {
+      dialogRef.afterClosed().subscribe((response: boolean | string) => {
+        if (response === false) {
           this.router.events
             .pipe(
-              filter(event => event instanceof GuardsCheckEnd),
+              filter((event): event is GuardsCheckEnd => event instanceof GuardsCheckEnd),
               take(1)
             )
             .subscribe({
-              next: (router: GuardsCheckEnd) => component.saveChanges(router.urlAfterRedirects)
+              next: (router: GuardsCheckEnd) => component.saveChanges(router.urlAfterRedirects),
+              error: ({ message }) => this.snackbar.error(message)
             });
         }
-        return;
       });
-      return dialogRef.afterClosed();
+
+      return dialogRef.afterClosed().pipe(
+        map((result: boolean) => result === true)
+      );
     }
+
+    // No pending changes, else show confirm dialog.
     return of(true);
   }
 }

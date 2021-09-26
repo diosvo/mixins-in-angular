1fe15e3d290bbbc60efeaa57e0f339d58a5020c5
Dios Vo <vtmn1212@gmail.com>
2021-09-26 20:15:50 +0700

    Features: - Reset changes button - Delete current user

--- src/app/functions/rxjs/crud-in-one-view/crud-in-one-view.component.ts
+++ src/app/functions/rxjs/crud-in-one-view/crud-in-one-view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SnackbarService } from '@lib/services/snackbar/snackbar.service';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 interface Info {
   name: string;
@@ -25,8 +26,8 @@ export class CrudInOneViewComponent implements OnInit, OnDestroy {
   })
 
   private readonly USER_STORAGE = 'user';
-  readonly hasChanges$ = new Observable<boolean>();
   readonly user$ = new BehaviorSubject<Info>({} as Info);
+  hasChanges$ = new Observable<boolean>();
   private destroy$ = new Subject<boolean>();
 
   constructor(
@@ -42,25 +43,41 @@ export class CrudInOneViewComponent implements OnInit, OnDestroy {
     return JSON.stringify(this.user$.value) !== JSON.stringify({});
   }
 
-  private watchForChanges(): void {
-
-  }
-
   getUser(): void {
     const user = JSON.parse(localStorage.getItem(this.USER_STORAGE));
     if (!!user) {
       this.user$.next(user);
+      this.watchForChanges();
       this.form.patchValue(user);
     }
     return;
+  }
+
+  private watchForChanges(): void {
+    this.hasChanges$ = combineLatest([this.user$, this.form.valueChanges])
+      .pipe(
+        map(([prev, next]) => JSON.stringify(prev) !== JSON.stringify(next)),
+        startWith(false)
+      );
   }
 
   onCreate(): void {
     this.user$.next(DEFAULT_VALUES);
   }
 
+  onReset(): void {
+    this.form.patchValue(this.user$.value);
+  }
+
+  onDelete(): void {
+    this.clearStorage();
+    this.form.reset();
+    this.user$.next({} as Info);
+  }
+
   onSave(): void {
     this.user$.next(this.form.value);
+    this.watchForChanges();
     localStorage.setItem(this.USER_STORAGE, JSON.stringify(this.user$.value));
     this.snackbar.success('User has been saved.');
   }

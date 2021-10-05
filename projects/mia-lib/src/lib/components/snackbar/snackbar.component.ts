e0aaeba5601d3e2e4f5fd2b01f8cc476fd82f878
Dios Vo <vtmn1212@gmail.com>
2021-10-05 16:14:19 +0700

    Refactor/ Scale severity color: - Apply for alert message, snackbar and chip

--- projects/mia-lib/src/lib/components/snackbar/snackbar.component.ts
+++ projects/mia-lib/src/lib/components/snackbar/snackbar.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./snackbar.component.scss']
 })
 export class SnackbarComponent {
 

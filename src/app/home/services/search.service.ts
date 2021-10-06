a68743b1c1fd0474324d6811f64f43f5f5a0371d
Dios Vo <vtmn1212@gmail.com>
2021-10-06 21:03:11 +0700

    Remove web module

--- src/app/home/services/search.service.ts
+++ src/app/home/services/search.service.ts
@@ -1,69 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { catchError, finalize, map, shareReplay } from 'rxjs/operators';
-import { IGroupValue } from '../models/search.model';
-import { EUrl } from '../models/url.enum';
+import { IGroupValue } from '@home/models/search.model';
+import { EUrl } from '@home/models/url.enum';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class SearchService {
-  // Web is temporarily inactive
-  private funcLoading$ = new BehaviorSubject<boolean>(true);
 
   uiComponentsList$: Observable<Array<IGroupValue>> =
-    this.http.get<Array<IGroupValue>>(this.getDataFrom(EUrl.COMPONENT))
+    this.http.get<Array<IGroupValue>>(this.path(EUrl.COMPONENT))
       .pipe(
         map((data: Array<IGroupValue>) => data.map(item => ({ ...item, groupUrl: EUrl.COMPONENT }))),
         shareReplay()
       );
 
-  webUiList$ = this.http.get<Array<IGroupValue>>(this.getDataFrom(EUrl.WEB))
-    .pipe(
-      map((data: Array<IGroupValue>) => data.map(item => ({ ...item, groupUrl: EUrl.WEB }))),
-      shareReplay(),
-      catchError(_ => of(null)),
-    );
-
-  functionsList$ = this.http.get<Array<IGroupValue>>(this.getDataFrom(EUrl.FUNCTION))
+  functionsList$ = this.http.get<Array<IGroupValue>>(this.path(EUrl.FUNCTION))
     .pipe(
       map((data: Array<IGroupValue>) => data.map(item => ({ ...item, groupUrl: EUrl.FUNCTION }))),
-      map(group => group.sort((prev, next) => prev.groupName < next.groupName ? -1 : 1)),
-      shareReplay(),
-      catchError(_ => of(null)),
-      finalize(() => this.funcLoading$.next(false)),
+      shareReplay()
     );
 
   constructor(
     private http: HttpClient
-  ) {
-    this.funcLoading$.next(true);
-  }
+  ) { }
 
-  getFuncLoading(): BehaviorSubject<boolean> {
-    return this.funcLoading$;
-  }
 
-  onFilter(session: EUrl, q: string): Observable<Array<IGroupValue>> {
-    return this.uiComponentsList$
-      .pipe(
-        map((items: Array<IGroupValue>) =>
-          items
-            .map(item => ({
-              groupName: item.groupName,
-              groupDetails: item.groupDetails.filter(
-                details => details.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-              ),
-              groupUrl: session
-            }))
-            .filter(item => item.groupDetails.length > 0)
-        )
-      );
-  }
-
-  private getDataFrom(url: EUrl): string {
+  private path(url: EUrl): string {
     return `/assets/backend/list-items/${url}.json`;
   }
 }
--- src/app/web-ui/list-web-ui.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-list-web-ui',
-  templateUrl: './list-web-ui.component.html'
-})
-export class ListWebUiComponent { }

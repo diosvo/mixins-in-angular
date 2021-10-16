4dbd4863b3c80592b0e1a64a3682588068552130
Dios Vo <vtmn1212@gmail.com>
2021-10-16 23:22:23 +0700

    Utils: window.localStorage object

--- projects/mia-lib/src/lib/utils/utils.ts
+++ projects/mia-lib/src/lib/utils/utils.ts
@@ -1,6 +1,15 @@
+import { inject, InjectionToken } from '@angular/core';
+
 const coerceToArray = <T>(value: T | Array<T>): Array<T> => (
   Array.isArray(value) ? value : [value]
 );
 
-export { coerceToArray };
+const LOCAL_STORAGE = new InjectionToken<Storage>(
+  'An abstraction over window.localStorage object',
+  {
+    factory: () => inject(Window).localStorage
+  }
+);
+
+export { coerceToArray, LOCAL_STORAGE };
 

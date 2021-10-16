74eb6f06f5d1d2fb90be012a3cf0082d06539e57
Dios Vo <vtmn1212@gmail.com>
2021-10-16 16:50:19 +0700

    mergeMap operator

--- src/app/functions/rxjs/basic-operators/basic-operators.component.ts
+++ src/app/functions/rxjs/basic-operators/basic-operators.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable, Subject, tap, zip } from 'rxjs';
+import { map, mergeMap, Observable, Subject, take, tap, zip } from 'rxjs';
 
-type Durum = ['flat bread', 'meat', 'sauce', 'tomato', 'cabbage']
+type Durum = ['flat bread', 'meat', 'sauce', 'tomato', 'cabbage'];
+
+interface Order {
+  amount: number;
+  customerId: number;
+}
+
+interface Product {
+  product: Durum;
+  customerId: number;
+}
 
 let flatBreadCounter = 0;
 let meatCounter = 0;
 let sauceCounter = 0;
 let tomatoCounter = 0;
 let cabbageCounter = 0;
+let customerId = 0;
 
 @Component({
   selector: 'app-basic-operators',
@@ -17,7 +28,9 @@ let cabbageCounter = 0;
 export class BasicOperatorsComponent implements OnInit {
 
   durum$: Observable<Durum>;
+  delivery$: Observable<Product>;
 
+  _order = new Subject<Order>()
   _flatBread = new Subject<'flat bread'>();
   _meat = new Subject<'meat'>();
   _sauce = new Subject<'sauce'>();
@@ -50,5 +63,22 @@ export class BasicOperatorsComponent implements OnInit {
     ).pipe(
       tap(durum => console.log('Enjoy!', durum))
     );
+
+    this.delivery$ = this._order.pipe(
+      tap(order => console.log('New Order', order)),
+      mergeMap(({ amount, customerId }) =>
+        this.durum$.pipe(
+          take(amount),
+          map(durum => ({ product: durum, customerId }))
+        )
+      ),
+      tap(product => console.log('Delivery Product', product))
+    );
+  }
+
+  dispatchOrder(): void {
+    const amount = Math.floor(Math.random() * 3) + 1;
+    ++customerId;
+    this._order.next({ amount, customerId });
   }
 }

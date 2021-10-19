64d5275754913e2927ec4264cbd4ae5131d6704c
Dios Vo <vtmn1212@gmail.com>
2021-10-19 20:35:40 +0700

    Algorithm: bubble-sort

--- src/app/functions/algorithm/self-learning/self-learning.component.ts
+++ src/app/functions/algorithm/self-learning/self-learning.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-self-learning',
   templateUrl: './self-learning.component.html',
   styleUrls: ['./self-learning.component.scss']
 })
-export class SelfLearningComponent implements OnInit {
+export class SelfLearningComponent {
+
+  numbers = [1, 5, 4, 2, 3];
 
   constructor() { }
 
-  ngOnInit(): void {
+  bubbleSort(numbers: Array<number>): void {
+    for (let idx = 0; idx < numbers.length; idx++) {
+      /* if the prev numb is greater than next numb, swap it */
+      if (numbers[idx] > numbers[idx + 1]) {
+        const temp = numbers[idx];
+        numbers[idx] = numbers[idx + 1];
+        numbers[idx + 1] = temp;
+      }
+    }
   }
-
 }

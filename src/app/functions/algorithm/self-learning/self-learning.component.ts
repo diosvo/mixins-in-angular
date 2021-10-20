9129aa943a339cc1a17847306295a533a579a6a5
Dios Vo <vtmn1212@gmail.com>
2021-10-20 21:41:39 +0700

    Algorithm: insertion-sort

--- src/app/functions/algorithm/self-learning/self-learning.component.ts
+++ src/app/functions/algorithm/self-learning/self-learning.component.ts
@@ -11,14 +11,36 @@ export class SelfLearningComponent {
 
   constructor() { }
 
-  bubbleSort(numbers: Array<number>): void {
+  bubbleSort(numbers: Array<number>): Array<number> {
     for (let idx = 0; idx < numbers.length; idx++) {
-      /* if the prev numb is greater than next numb, swap it */
+      // if the prev numb is larger than next numb, swap it
       if (numbers[idx] > numbers[idx + 1]) {
         const temp = numbers[idx];
         numbers[idx] = numbers[idx + 1];
         numbers[idx + 1] = temp;
       }
+
+      return numbers;
     }
+  }
+
+  /**
+   * @advantage conceptualize & more useful in general algorithm
+   * @description start by index 0 -> sorted; everything after unsorted.
+   */
+
+  insertionSort(numbers: Array<number>): Array<number> {
+    for (let idx = 1; idx < numbers.length; idx++) {
+      let numberToInsert = numbers[idx]; // number is gonna be comparing
+      let trackIdx; // the inner counter
+
+      for (trackIdx = idx - 1; numbers[trackIdx] > numberToInsert && trackIdx >= 0; trackIdx--) {
+        numbers[trackIdx + 1] = numbers[trackIdx]; // move numbers to the right until find where to insert
+      }
+
+      numbers[trackIdx + 1] = numberToInsert; // do the insertion
+    }
+
+    return numbers;
   }
 }

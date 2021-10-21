325a0eb9ded244c2f8cab5e343d5124b475a173d
Dios Vo <vtmn1212@gmail.com>
2021-10-21 21:37:01 +0700

    Unit test for algorithms

--- src/app/functions/algorithm/self-learning/self-learning.component.spec.ts
+++ src/app/functions/algorithm/self-learning/self-learning.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SelfLearningComponent } from './self-learning.component';
 
 describe('SelfLearningComponent', () => {
@@ -8,18 +7,38 @@ describe('SelfLearningComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SelfLearningComponent ]
+      declarations: [SelfLearningComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SelfLearningComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Check algorithm: ', () => {
+    describe('Iterative Sorts', () => {
+      test('bubbleSort()', () => {
+        component.bubbleSort([1, 5, 4, 2, 3]);
+      });
+
+      test('insertionSort()', () => {
+        expect(component.insertionSort([1, 5, 4, 2, 3])).toEqual([1, 2, 3, 4, 5]);
+      });
+    });
+
+    describe('Recursion', () => {
+      test('nestedArray()', () => {
+        expect(component.nestedArray([1, 2, 3])).toEqual(6);
+        expect(component.nestedArray([1, [2], 3])).toEqual(6);
+        expect(component.nestedArray([[[[[[[[[5]]]]]]]]])).toEqual(5);
+        expect(component.nestedArray([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
+      });
+    });
   });
 });

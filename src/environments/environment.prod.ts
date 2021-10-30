cb294f8519dc7e02f300fd9f772555ac17a3a08e
Dios Vo <vtmn1212@gmail.com>
2021-10-30 18:16:20 +0700

    Fixed build failed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://diosvo.netlify.app'
+  apiUrl: 'https://diosvo.netlify.app',
+  jsonPlaceHolderUrl: 'https://jsonplaceholder.typicode.com/'
 };

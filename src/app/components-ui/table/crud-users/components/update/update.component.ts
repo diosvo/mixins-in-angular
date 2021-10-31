bc1a615ce12a8197c5109c0688ca07281e3b57dd
Dios Vo <vtmn1212@gmail.com>
2021-10-31 17:01:50 +0700

    User details: detect changes

--- src/app/components-ui/table/crud-users/components/update/update.component.ts
+++ src/app/components-ui/table/crud-users/components/update/update.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { IUser } from '@lib/models/user';
 import { SnackbarService } from '@lib/services/snackbar/snackbar.service';
 import { UsersService } from '@lib/services/users/users.service';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { combineLatest, first, map, Observable, startWith, Subject, takeUntil } from 'rxjs';
 
 type User = Partial<IUser>;
 
@@ -14,16 +13,16 @@ type User = Partial<IUser>;
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent implements OnInit, OnDestroy {
-  
+
   user$: Observable<User>;
   destroy$ = new Subject<boolean>();
 
   user_id: number;
   isValid = true;
+  hasChanged = false;
   user = new FormControl({ name: '', email: '' });
 
   constructor(
-    private readonly route: ActivatedRoute,
     private readonly userService: UsersService,
     private readonly snackbar: SnackbarService,
   ) { }
@@ -32,6 +31,19 @@ export class UpdateComponent implements OnInit, OnDestroy {
     this.user$ = this.userService.currentUser$.pipe(
       takeUntil(this.destroy$)
     );
+    this.watchForFormChanged();
+  }
+
+  private watchForFormChanged(): void {
+    combineLatest([
+      this.user$.pipe(map(({ name, email }) => ({ name, email })), first()),
+      this.user.valueChanges
+    ])
+      .pipe(
+        map(([prev, next]) => JSON.stringify(prev) !== JSON.stringify(next)),
+        startWith(false)
+      )
+      .subscribe(response => this.hasChanged = response);
   }
 
   onFormChanged(data: { name: string, email: string }): void {

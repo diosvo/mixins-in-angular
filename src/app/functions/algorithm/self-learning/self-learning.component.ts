f9b5cd8b6369f2a95eacc53ba6c18f15474427cc
Dios Vo <vtmn1212@gmail.com>
2021-11-08 20:49:37 +0700

    Algorithm - Trees: binary-search-tree

--- src/app/functions/algorithm/self-learning/self-learning.component.ts
+++ src/app/functions/algorithm/self-learning/self-learning.component.ts
@@ -287,3 +287,57 @@ export class ArrayList {
     this.length--;
   }
 }
+
+export class Tree {
+  root = null;
+
+  add(value: unknown): unknown {
+    // some logic around if this is the root
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true) { // keep running. there's always gonna be somewhere to put the add ot to put this new node
+        if (current.value > value) {
+          // go left
+
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+
+        } else {
+          // go right
+
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+
+        }
+      }
+    }
+
+    return this;
+  }
+
+  toObject(): void {
+    return this.root;
+  }
+}
+
+class Node {
+  value: unknown;
+  left = null;
+  right = null;
+
+  constructor(value: unknown) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}

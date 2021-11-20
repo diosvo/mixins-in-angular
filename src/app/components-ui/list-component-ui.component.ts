929c2d61b68116a9027ea3b053fffb5d6193a72d
Dios Vo <vtmn1212@gmail.com>
2021-11-20 21:24:00 +0700

    remove: set default demo

--- src/app/components-ui/list-component-ui.component.ts
+++ src/app/components-ui/list-component-ui.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IGroupValue } from '@home/models/search.model';
 import { EComponentUI } from '@home/models/url.enum';
 import { SearchService } from '@home/services/search.service';
 import { combineLatest, Observable, Subject, throwError } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, filter, map, startWith, takeUntil, tap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, map, startWith, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'list-component-ui',
@@ -43,17 +43,8 @@ export class ListComponentUiComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
-    this.setDefault();
+    this.watchForQueryParams();
     this.onFilters();
-  }
-
-  private setDefault(): void {
-    this.router.events.pipe(
-      filter((event): event is NavigationStart => event instanceof NavigationStart),
-    ).subscribe((res: any) => {
-      console.log(res);
-      
-    });
   }
 
   /**

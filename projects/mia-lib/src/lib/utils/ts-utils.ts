09748317364d9096947fa5fb744e28bb1d673c98
Dios Vo <vtmn1212@gmail.com>
2021-12-09 20:56:34 +0700

    feat: new util - FilterConditionally
    create a new type based on type conditionally

--- projects/mia-lib/src/lib/utils/ts-utils.ts
+++ projects/mia-lib/src/lib/utils/ts-utils.ts
@@ -5,3 +5,18 @@
 type UnCapitalizeKeys<T> = {
   [P in keyof T as `${Uncapitalize<string & P>}`]: T[P];
 }
+
+/** 
+ * @returns new type - filter object-based types conditionally.
+ */
+
+type MarkUnwantedTypesAsNever<Source, Condition> = {
+  [K in keyof Source]: Source[K] extends Condition ? K : never
+}[keyof Source];
+
+type FilterConditionally<Source, Condition> = Pick<Source, MarkUnwantedTypesAsNever<Source, Condition>>;
+
+/*------ EXPORT HERE -----*/
+
+export { };
+

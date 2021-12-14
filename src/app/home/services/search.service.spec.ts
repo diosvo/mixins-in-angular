9521c044efbd9373feaffd7b56258c2a8ec5ab20
Dios Vo <vtmn1212@gmail.com>
2021-12-14 21:37:01 +0700

    test (search-service)

--- src/app/home/services/search.service.spec.ts
+++ src/app/home/services/search.service.spec.ts
@@ -1,16 +1,58 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { IGroupValue } from '@home/models/search.model';
+import { EUrl } from '@home/models/url.enum';
 import { SearchService } from './search.service';
+
+const ui_comps: Array<IGroupValue> = [
+  {
+    groupName: 'button',
+    groupUrl: EUrl.COMPONENT,
+    groupDetails: [
+      {
+        name: 'Micro Interaction',
+        route: 'micro-interaction',
+        description: 'GSAP'
+      }
+    ]
+  }
+];
+
+const funcs: Array<IGroupValue> = [
+  {
+    groupName: 'rxjs',
+    groupUrl: EUrl.FUNCTION,
+    groupDetails: [
+      {
+        name: 'Data Composition #ng-conf',
+        route: 'data-composition-ng-conf',
+        description: 'Without subscription, improve performance'
+      }
+    ]
+  },
+];
 
 describe('SearchService', () => {
   let service: SearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(SearchService);
   });
 
-  it('should be created', () => {
+  test('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('uiComponentsList$', () => {
+    service.uiComponentsList$.subscribe((response: Array<IGroupValue>) => expect(response).toEqual(ui_comps));
+  });
+
+  test('functionsList$', () => {
+    service.functionsList$.subscribe((response: Array<IGroupValue>) => expect(response).toEqual(funcs));
   });
 });

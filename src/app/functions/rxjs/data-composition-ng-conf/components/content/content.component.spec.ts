7cb5f403de1475be4264f1152cd2e81bf7b579c0
Dios Vo <vtmn1212@gmail.com>
2021-12-15 22:37:38 +0700

    test (product details) & code clean up

--- src/app/functions/rxjs/data-composition-ng-conf/components/content/content.component.spec.ts
+++ src/app/functions/rxjs/data-composition-ng-conf/components/content/content.component.spec.ts
@@ -1,22 +1,46 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { IProduct } from '@lib/models/product';
+import { ProductsService } from '@lib/services/products/products.service';
+import { of } from 'rxjs';
 import { ContentComponent } from './content.component';
+
+const product: IProduct = {
+  productImage: 'https://picsum.photos/400?image=659',
+  productId: 1000,
+  productName: 'delightful chocolate Ilise',
+  productStock: 81,
+  productPrice: 23488.67,
+  productSalePrice: 23488.67,
+  rating: 1,
+  categoryId: 1
+};
 
 describe('ContentComponent', () => {
   let component: ContentComponent;
   let fixture: ComponentFixture<ContentComponent>;
+  const service = {
+    selected$: of(product),
+    productSelectedAction: of(1000)
+  };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [ContentComponent],
       imports: [
         HttpClientModule,
         RouterTestingModule,
+      ],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: service
+        }
       ]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContentComponent);
@@ -24,7 +48,19 @@ describe('ContentComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('should call displayProduct()', () => {
+    test('should show product details when user select any product', () => {
+      component.displayProduct(product);
+      expect(component.pageTitle).toBe(`Product Details ➡ ${product.productName}`);
+    });
+
+    test('should show no product found when there has no product', () => {
+      component.displayProduct(undefined);
+      expect(component.pageTitle).toBe('No product found');
+    });
   });
 });

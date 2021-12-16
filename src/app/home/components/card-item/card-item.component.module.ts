cfae3d98d222744b712f92a51acb322d747b1d45
Dios Vo <vtmn1212@gmail.com>
2021-12-16 14:37:33 +0700

    refactor (card-item): - confusion between component and test file - update UT coverage (done)

--- src/app/home/components/card-item/card-item.component.module.ts
+++ src/app/home/components/card-item/card-item.component.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { CardItemComponent } from './card-item.component.spec';
+import { CardItemComponent } from './card-item.component';
 
 @NgModule({
   declarations: [CardItemComponent],

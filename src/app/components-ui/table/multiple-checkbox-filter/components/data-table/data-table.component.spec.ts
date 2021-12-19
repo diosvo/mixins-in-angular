554a19a1d7118b4738eadb340fac8f1875e16cad
Dios Vo <vtmn1212@gmail.com>
2021-12-19 12:58:10 +0700

    test (github-issues-table)

--- src/app/components-ui/table/multiple-checkbox-filter/components/data-table/data-table.component.spec.ts
+++ src/app/components-ui/table/multiple-checkbox-filter/components/data-table/data-table.component.spec.ts
@@ -1,6 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertModule } from '@lib/components/alert/alert.module';
 import { CustomTableModule } from '@lib/components/custom-table/custom-table.module';
@@ -21,6 +24,10 @@ describe('DataTableComponent', () => {
         HttpClientModule,
         ReactiveFormsModule,
         BrowserAnimationsModule,
+
+        MatInputModule,
+        MatSelectModule,
+        MatFormFieldModule,
       ]
     })
       .compileComponents();
--- src/app/components-ui/table/multiple-checkbox-filter/components/search-filter/search-filter.component.spec.ts
+++ src/app/components-ui/table/multiple-checkbox-filter/components/search-filter/search-filter.component.spec.ts
@@ -1,18 +1,26 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchFilterComponent } from './search-filter.component';
 
-describe.skip('SearchFilterComponent', () => {
+describe('SearchFilterComponent', () => {
   let component: SearchFilterComponent;
   let fixture: ComponentFixture<SearchFilterComponent>;
 
-  beforeEach(waitForAsync(async () => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      declarations: [SearchFilterComponent],
       imports: [
-        FormBuilder,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+
+        MatInputModule,
+        MatSelectModule,
+        MatFormFieldModule,
       ],
-      declarations: [SearchFilterComponent]
     })
       .compileComponents();
   }));
@@ -22,12 +30,27 @@ describe.skip('SearchFilterComponent', () => {
     component = fixture.componentInstance;
     component.filterForm = new FormGroup({
       query: new FormControl(''),
-      state: new FormControl('open'),
+      state: new FormControl(['closed']),
     });
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    jest.spyOn(component, 'ngOnDestroy');
+    fixture.destroy();
+  });
+
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('watch for filters change', fakeAsync(() => {
+    jest.spyOn(component.filters, 'emit');
+
+    component.ngOnInit();
+    component.filterForm.patchValue({ query: '1', state: ['open'] });
+    tick(100);
+
+    component.filterForm.valueChanges.subscribe(response => expect(component.filters.emit).toBeCalledWith(response));
+  }));
 });

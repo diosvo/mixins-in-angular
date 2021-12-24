57621dd786700449bc0ad36ac6668b7ec8ecc51c
Dios Vo <vtmn1212@gmail.com>
2021-12-25 00:40:21 +0700

    feat (ts-utils): predicate combinator

--- projects/mia-lib/src/lib/utils/ts-utils.ts
+++ projects/mia-lib/src/lib/utils/ts-utils.ts
@@ -16,6 +16,25 @@ type MarkUnwantedTypesAsNever<Source, Condition> = {
 
 type FilterConditionally<Source, Condition> = Pick<Source, MarkUnwantedTypesAsNever<Source, Condition>>;
 
+/** 
+ * @description predicate combinator
+ */
+
+type PredicateFn = (value: unknown, index?: number) => boolean;
+type ProjectionFn = (value: unknown, index?: number) => unknown;
+
+function OR(...predicates: Array<PredicateFn>): PredicateFn {
+  return (value) => predicates.some((predicate) => predicate(value));
+}
+
+function AND(...predicates: Array<PredicateFn>): PredicateFn {
+  return (value) => predicates.every((predicate) => predicate(value));
+}
+
+function NOT(...predicates: Array<PredicateFn>): PredicateFn {
+  return (value) => predicates.every((predicate) => predicate(value));
+}
+
 /*------ EXPORT HERE -----*/
 
 export { };

c4dbfa8668a6e8a5426198af19e1f01f4f740c6a
Dios Vo <vtmn1212@gmail.com>
2021-12-29 15:23:02 +0700

    feat (auth.interceptor): more powerful - handle errors - write unit test

--- src/app/auth/utils/auth.interceptor.spec.ts
+++ src/app/auth/utils/auth.interceptor.spec.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpRequest, HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from '@auth/services/auth.service';
@@ -7,18 +7,19 @@ import { AuthInterceptor, AuthInterceptorProvider } from './auth.interceptor';
 
 describe('AuthInterceptor', () => {
   let interceptor: AuthInterceptor;
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-        AuthService,
         AuthInterceptor,
         AuthInterceptorProvider,
       ]
     });
 
     interceptor = TestBed.inject(AuthInterceptor);
+    service = TestBed.inject(AuthService);
   });
 
   test('should add an authorization header', () => {
@@ -36,6 +37,41 @@ describe('AuthInterceptor', () => {
     interceptor.intercept(request, next).subscribe(() => {
       expect(response.headers.has('authorization')).toBe(true);
       expect(response.headers.get('authorization')).toEqual('token_string');
+    });
+  });
+
+  describe('should handle error if an error related to', () => {
+    test('unauthorized', () => {
+      const request = new HttpRequest('GET', '/test');
+      const next = {
+        handle: () => {
+          return new Observable(subscriber => {
+            subscriber.error({ status: HttpStatusCode.Unauthorized });
+            subscriber.complete();
+          });
+        }
+      } as any;
+
+      interceptor.intercept(request, next).subscribe(() => {
+        expect(service.logout).toBeCalled();
+      });
+    });
+
+    test('another issues', () => {
+      const request = new HttpRequest('GET', '/test');
+      const next = {
+        handle: () => {
+          return new Observable(subscriber => {
+            subscriber.error({
+              status: HttpStatusCode.BadRequest,
+              message: 'Bad request'
+            });
+            subscriber.complete();
+          });
+        }
+      } as any;
+
+      interceptor.intercept(request, next).subscribe(message => expect(message).toBe('Bad request'));
     });
   });
 });

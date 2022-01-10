9fc148ccf6ad249f49eefd1755bbf135e5f313b9
Dios Vo <vtmn1212@gmail.com>
2022-01-10 14:32:00 +0700

    test (activated-params)

--- projects/mia-lib/src/lib/services/activated-params/activated-params.service.spec.ts
+++ projects/mia-lib/src/lib/services/activated-params/activated-params.service.spec.ts
@@ -1,18 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ActivatedParamsService } from './activated-params.service';
+
+class MockRouter {
+  public ne = new NavigationEnd(0, 'http://localhost:4200/ui-components', 'http://localhost:4200/ui-components');
+  public events = new Observable(observer => {
+    observer.next(this.ne);
+    observer.complete();
+  });
+}
 
 describe('ActivatedParamsService', () => {
   let service: ActivatedParamsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule]
+      providers: [
+        {
+          provide: Router,
+          useClass: MockRouter
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            firstChild: ActivatedRoute,
+            outlet: 'primary',
+          }
+        }
+      ]
     });
+
     service = TestBed.inject(ActivatedParamsService);
   });
 
-  it('should be created', () => {
+  test('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('isShallowEqual()', () => {
+    test('returns true if the URLs are equal', () => {
+      expect(service['isShallowEqual']('/ui-components', '/ui-components')).toBe(true);
+    });
+
+    test('returns true if the URLs are NOT equal (navigate to another page)', () => {
+      expect(service['isShallowEqual']('/ui-components', '/functions')).toBe(false);
+    });
   });
 });

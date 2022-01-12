fc7242564cb17bcf9712c1f1c00b66bb485d8b47
Dios Vo <vtmn1212@gmail.com>
2022-01-12 10:37:38 +0700

    style: - import temp data. - create style (responsive included)

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
+  styleUrls: ['./core.component.scss']
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  list$ = of([
+    {
+      name: 'algorithms',
+      image: 'assets/images/core/algorithms.png'
+    },
+    {
+      name: 'data structures',
+      image: 'assets/images/core/data-structures.png'
+    }
+  ]).pipe(delay(500));
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
 
-
 @NgModule({
-  declarations: [
-    CoreComponent
-  ],
+  declarations: [CoreComponent],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+
+    MatProgressSpinnerModule
   ]
 })
 export class CoreModule { }

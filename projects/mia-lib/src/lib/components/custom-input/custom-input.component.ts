1bdb3724d2f63391eac6b9c40ee1aacfd83c3317
Dios Vo <vtmn1212@gmail.com>
2022-02-07 21:54:05 +0700

    fix (custom-input): - class extends accessor connector - type is string as default; remove type string and required property in fields that are using

--- projects/mia-lib/src/lib/components/custom-input/custom-input.component.ts
+++ projects/mia-lib/src/lib/components/custom-input/custom-input.component.ts
@@ -1,8 +1,7 @@
-import { Component, forwardRef, Input } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator } from '@angular/forms';
+import { Component, forwardRef, Injector, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatFormFieldAppearance } from '@angular/material/form-field';
-
-type InputType = 'string' | 'number';
+import { FormControlValueAccessorConnector } from '../form-control-value-accessor-connector/form-control-value-accessor-connector.component';
 
 @Component({
   selector: 'custom-input',
@@ -13,70 +12,21 @@ type InputType = 'string' | 'number';
       useExisting: forwardRef(() => CustomInputComponent),
       multi: true
     },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => CustomInputComponent),
-      multi: true
-    }
   ]
 })
 
-export class CustomInputComponent implements ControlValueAccessor, Validator {
+export class CustomInputComponent extends FormControlValueAccessorConnector {
   // Field
   @Input() style = 'width: 100%';
   @Input() appearance: MatFormFieldAppearance | 'none' = 'outline';
 
   // Input
-  @Input() type!: string;
+  @Input() type = 'string';
   @Input() label?: string;
-  @Input() pattern?: string;
   @Input() placeholder?: string;
-  @Input() required = false;
-  @Input() readonly?: boolean;
+  @Input() readonly: boolean = false;
 
-  disabled = false;
-
-  /** Callback when the value is changing **/
-  onChange: (value: InputType) => void;
-  /** Callback when the input is accessed **/
-  onTouched: () => void;
-
-  private _value: InputType;
-  control: FormControl;
-
-  get value(): InputType {
-    return this._value;
-  }
-
-  set value(v: InputType) {
-    if (v !== this._value) {
-      this._value = v;
-      this.onChange(v);
-    }
-  }
-
-  writeValue(value: InputType): void {
-    if (value !== undefined) {
-      this._value = value;
-    }
-  }
-
-  registerOnChange(fn: (_: InputType) => {}): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: () => {}): void {
-    this.onTouched = fn;
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
-  validate(control: FormControl): FormControl {
-    if (!this.control) {
-      this.control = control;
-      return null;
-    }
+  constructor(injector: Injector) {
+    super(injector);
   }
 }

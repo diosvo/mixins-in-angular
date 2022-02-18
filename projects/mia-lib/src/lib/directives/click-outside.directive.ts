607ce3beff737155401ed666e431170224862246
Dios Vo <vtmn1212@gmail.com>
2022-02-18 22:31:55 +0700

    feat: click outside directive

---
+++ projects/mia-lib/src/lib/directives/click-outside.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, EventEmitter, HostListener, NgModule, Output } from '@angular/core';
+
+@Directive({
+  selector: '[click-outside]'
+})
+export class ClickOutsideDirective {
+
+  @Output() clickOutside = new EventEmitter<MouseEvent>();
+
+  constructor(private readonly elementRef: ElementRef) { }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+
+    if (target && !this.elementRef.nativeElement.contains(target)) {
+      this.clickOutside.emit(event);
+    }
+    throw new Error('Some errors occurred.');
+  }
+}
+
+@NgModule({
+  declarations: [ClickOutsideDirective],
+  exports: [ClickOutsideDirective]
+})
+export class ClickOutsideDirectiveModule { }
+
+/** 
+ * @usage [click-outside]"
+ */

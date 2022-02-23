4559163a5fd50f5fc19f0915432fa63c1a7a5b0d
Dios Vo <vtmn1212@gmail.com>
2022-02-23 23:07:38 +0700

    feat (view-article): implements search with pipe

--- src/app/components-ui/table/view-article-page-state/view-article-page-state.module.ts
+++ src/app/components-ui/table/view-article-page-state/view-article-page-state.module.ts
@@ -4,7 +4,10 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterModule, Routes } from '@angular/router';
 import { AlertModule } from '@lib/components/alert/alert.module';
 import { CustomButtonModule } from '@lib/components/custom-button/custom-button.module';
+import { CustomInputModule } from '@lib/components/custom-input/custom-input.module';
 import { CustomTableModule } from '@lib/components/custom-table/custom-table.module';
+import { BindQueryParamDirectiveModule } from '@lib/directives/bind-query-param.directive';
+import { FilterPipeModule } from 'projects/mia-lib/src/lib/pipes/filter.pipe';
 import { ListArticlesComponent } from './components/list-articles/list-articles.component';
 import { ViewArticlePageStateComponent } from './components/view-article-page-state/view-article-page-state.component';
 
@@ -35,8 +38,11 @@ const routes: Routes = [
     RouterModule.forChild(routes),
 
     AlertModule,
+    FilterPipeModule,
+    CustomInputModule,
     CustomTableModule,
     CustomButtonModule,
+    BindQueryParamDirectiveModule,
 
     MatProgressBarModule
   ]

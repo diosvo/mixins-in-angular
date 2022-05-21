7f346879015869177d4263ac83f6255950394cc2
Dios Vo <vtmn1212@gmail.com>
2022-05-21 23:00:39 +0700

    refactor (lookup-users)
    - remove unused component
    - text empty should not have hover state

--- src/app/components-ui/form/lookup-users/lookup-users.module.ts
+++ src/app/components-ui/form/lookup-users/lookup-users.module.ts
@@ -8,7 +8,6 @@ import { TrackByKeyDirectiveModule } from '@lib/directives/track-by-key.directiv
 import { UsersService } from '@lib/services/users/users.service';
 import { FilterPipeModule } from 'projects/mia-lib/src/lib/pipes/filter.pipe';
 import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
-import { DataTableComponent } from './components/data-table/data-table.component';
 import { SearchPageComponent } from './components/search-page/search-page.component';
 import { SelectOptionComponent } from './components/select-option/select-option.component';
 import { AutocompleteContentDirective } from './utils/autocomplete-content.directive';
@@ -16,7 +15,6 @@ import { AutocompleteDirective } from './utils/autocomplete.directive';
 
 @NgModule({
   declarations: [
-    DataTableComponent,
     SearchPageComponent,
     SelectOptionComponent,
     AutocompleteComponent,

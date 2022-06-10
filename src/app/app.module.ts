4248440dbe0d0f00e0ba60a08a58b0c7a9245f53
Dios Vo <vtmn1212@gmail.com>
2022-06-10 12:05:53 +0700

    core: init angular/firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FakeBackendProvider } from '@backend/api/fake-be';
 import { UnsavedChangesDialogModule } from '@lib/components/unsaved-changes-dialog/unsaved-changes-dialog.module';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterModule } from './home/components/footer/footer.module';
@@ -14,9 +21,7 @@ import { ToolbarModule } from './home/components/toolbar/toolbar.module';
 import { MonitorInterceptor } from './interceptors/monitor.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -28,7 +33,15 @@ import { MonitorInterceptor } from './interceptors/monitor.interceptor';
     FooterModule,
     ToolbarModule,
     MatSnackBarModule,
-    UnsavedChangesDialogModule
+    UnsavedChangesDialogModule,
+
+    /* init firebase config */
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage())
   ],
   providers: [
     FakeBackendProvider,
@@ -40,7 +53,9 @@ import { MonitorInterceptor } from './interceptors/monitor.interceptor';
       provide: HTTP_INTERCEPTORS,
       useClass: MonitorInterceptor,
       multi: true
-    }
+    },
+    ScreenTrackingService,
+    UserTrackingService
   ],
   bootstrap: [AppComponent]
 })

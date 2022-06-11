cb77885f6986b204c8795fc1e898eddff7f6e342
Dios Vo <vtmn1212@gmail.com>
2022-06-11 12:16:58 +0700

    core: add apiKey of firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:942092830389:web:a72951b483792c243d60fc',
     databaseURL: 'https://mixins-in-angular-default-rtdb.firebaseio.com',
     storageBucket: 'mixins-in-angular.appspot.com',
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: 'AIzaSyA7ZDE4xMjyJnhMBg1rHY-RTI4lB_sm6yE',
     authDomain: 'mixins-in-angular.firebaseapp.com',
     messagingSenderId: '942092830389',
   },

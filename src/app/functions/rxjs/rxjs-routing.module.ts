55250af5ef521d628a19073f8083e4274b75ac74
Dios Vo <vtmn1212@gmail.com>
2022-09-19 19:53:16 +0700

    remove data-composition-ng-conf components and related services

--- src/app/functions/rxjs/rxjs-routing.module.ts
+++ src/app/functions/rxjs/rxjs-routing.module.ts
@@ -2,11 +2,6 @@ import { Routes } from '@angular/router';
 
 export const RXJS_ROUTES: Routes = [
   {
-    path: 'data-composition-ng-conf',
-    loadChildren: () => import('./data-composition-ng-conf/data-composition-ng-conf-routing.module')
-      .then(({ DATA_COMPOSITION_ROUTES }) => DATA_COMPOSITION_ROUTES)
-  },
-  {
     path: 'advanced-caching',
     loadComponent: () => import('./advanced-caching/components/advanced-caching/advanced-caching.component')
       .then(({ AdvancedCachingComponent }) => AdvancedCachingComponent),

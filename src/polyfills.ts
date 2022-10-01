2a2eac372be93a9b9b2bc13e686a236359f32b67
Dios Vo <vtmn1212@gmail.com>
2022-10-01 14:18:45 +0700

    chore: add i18n package to support plural ref

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -63,6 +67,3 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-import * as buffer from 'buffer';
-(window as any).Buffer = buffer.Buffer;

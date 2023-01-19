b780e64d2acb9043177e9f0e2a6cb287d3cb742d
Dios Vo <vtmn1212@gmail.com>
2023-01-20 00:33:33 +0700

    fix(snackbar.component): ensure the UI does not break

--- projects/mia-lib/src/lib/components/snackbar/snackbar.component.ts
+++ projects/mia-lib/src/lib/components/snackbar/snackbar.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
 import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
 
 @Component({
   selector: 'app-snackbar',
   templateUrl: './snackbar.component.html',
   standalone: true,
+  imports: [CustomButtonComponent],
   styleUrls: ['./snackbar.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
